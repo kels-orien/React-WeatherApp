0eab33c35158fc15369749061272a31fa1847ea3
kels-orien <kelvinorien@gmail.com>
2017-06-21 07:04:34 +0100

    New Update

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var Example = React.createClass ({
   render: function () {
     return (
-
-      <h3>Example Component</h3>
+  <div>
+      <h3>Examples Component</h3>
+      <p>Welcome to the examples page!</p>
+      </div>
 );
   }
 });
